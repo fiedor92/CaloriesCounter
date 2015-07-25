10f964aa68348370eabf023cc8479b060b53b4fa
Maciej <appacmp@appacmps-MacBook-Pro.local>
2015-07-25 13:39:33 +0200

    Initial Commit

---
+++ CaloriesCounter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CaloriesCounter
+//
+//  Created by appacmp on 25/07/15.
+//  Copyright (c) 2015 appcamp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
